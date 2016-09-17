9ff630fb85edd18b53e5d34e1a6cbbd75c4e6de5
Richie <rmgirardin@gmail.com>
2016-09-16 22:09:35 -0400

    Updated code for discord.js v9

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const helpArray = [
 
 // Connecting Mouse Bot
 bot.on('ready', () => {
-	console.log("%s is servering %s channel(s) over %s server(s)", bot.user.username, bot.channels.length, bot.servers.length);
+	console.log("%s is servering %s channel(s) over %s server(s)", bot.user.username, bot.channels.size, bot.guilds.size);
 });
 
 // When the Mouse Bot disconnects
@@ -48,99 +48,99 @@ bot.on('disconnected', () => {
 });
 
 // Welcomes new members to the server
-bot.on('serverNewMember', (server, user) => {
-  bot.sendMessage(server.channels.get('name', 'general'), "Welcome, " + user.username + ". Please make sure to read the <#" + env.discord.rulesChannel + ">. If you ever need help, just use \"/help\" and I'll assist.");
-  console.log(user.username + ' joined the server: ' + server); // update console
+bot.on('guildMemberAdd', (guild, member) => {
+  bot.channels.sendMessage("Welcome, " + member.nickname + ". Please make sure to read the <#" + env.discord.rulesChannel + ">. If you need help, use \"/help\" and I'll assist.");
+  console.log(member.nickname + ' joined the server: ' + server); // update console
 });
 
 // ----- /COMMANDS ----- //
 
-bot.on('message', msg => {
+bot.on('message', message => {
 
     // Setting and checking for prefix
     const prefix = "/";
-    if(!msg.content.startsWith(prefix)) return;
-    if(msg.author.bot) return;
+    if(!message.content.startsWith(prefix)) return;
+    if(message.author.bot) return;
 
 	// ----- Dailies
-	if (msg.content.startsWith(prefix + 'dailies')) {
-		dayCheck(msg);
-		console.log('Gave ' + msg.author.username + ' the dailies update'); // update console
+	if (message.content.startsWith(prefix + 'dailies')) {
+		dayCheck(message);
+		console.log('Gave ' + message.author.username + ' the dailies update'); // update console
 	}
 
 	// ----- Character Seach
-	if (msg.content.startsWith(prefix + 'ch')) {
-		characterLookup(msg);
-		console.log(msg.author.username + ' asked me to search for character information'); // update console
+	if (message.content.startsWith(prefix + 'ch')) {
+		characterLookup(message);
+		console.log(message.author.username + ' asked me to search for character information'); // update console
 	}
 
     // ----- Rancor Raid Timer and Notifier
-    if (msg.content.startsWith(prefix + 'rancor')) {
+    if (message.content.startsWith(prefix + 'rancor')) {
         timer.start(env.rancorTimer*3600); // convert hours to seconds
         timer.on('start', function() {
-            bot.sendMessage(env.discord.annChannel, "The Rancor Raid has been started. Only do 0 damage for 24 hours. See the <#" + env.discord.rulesChannel + ">");
+            message.channel.sendMessage("The Rancor Raid has been started. Only do 0 damage for 24 hours. See the <#" + env.discord.rulesChannel + ">");
         })
         timer.on('end', function() {
-            bot.sendMessage(env.discord.annChannel, "It's been 24 hours since the raid has started. Everyone can attack like normal!");
+            message.channel.sendMessage("It's been 24 hours since the raid has started. Everyone can attack like normal!");
         })
-        console.log(msg.author.username + ' has started the Rancor Raid timer') // update console
+        console.log(message.author.username + ' has started the Rancor Raid timer') // update console
     }
 
     // ----- Timer
-    if (msg.content.startsWith(prefix + 'tstart')) {
-        var messageSubstring = msg.content.substr(7);
+    if (message.content.startsWith(prefix + 'tstart')) {
+        var messageSubstring = message.content.substr(7);
         var messageTrimmed = messageSubstring.trim();
         var minutesToSeconds = 60*parseInt(messageTrimmed);
         timer.start(parseInt(minutesToSeconds));
-        bot.sendMessage(msg, "A timer has been started for " + messageTrimmed + " minute(s).");
+        message.channel.sendMessage("A timer has been started for " + messageTrimmed + " minute(s).");
         timer.on('end', function() {
-            bot.sendMessage(msg, "*DING! DING! DING!*" + '\n' + "Your timer has ended.");
+            message.channel.sendMessage("*DING! DING! DING!*" + '\n' + "Your timer has ended.");
         })
-        console.log(msg.author.username + ' started a timer'); // update console
+        console.log(message.author.username + ' started a timer'); // update console
     }
 
 	// ----- Rules
-	if (msg.content.startsWith(prefix + "rules")) {
-		bot.sendMessage(msg, "The rules are updated in the <#" + env.discord.rulesChannel + "> channel.");
-		console.log(msg.author.username + " asked me about the rules"); // update console
+	if (message.content.startsWith(prefix + "rules")) {
+		message.channel.sendMessage("The rules are updated in the <#" + env.discord.rulesChannel + "> channel.");
+		console.log(message.author.username + " asked me about the rules"); // update console
 	}
 
 	// ----- What's my name?
-	if (msg.content.startsWith(prefix + "name")) {
-		bot.sendTTSMessage(msg, "I love it when you call me big pop-pa!");
-		console.log("I told " + msg.author.username + " what I like to be called"); // update console
+	if (message.content.startsWith(prefix + "name")) {
+		message.channel.sendTTSMessage("I love it when you call me big pop-pa!");
+		console.log("I told " + message.author.username + " what I like to be called"); // update console
 	}
 
 	// ----- Time
-	if (msg.content.startsWith(prefix + "time")) {
+	if (message.content.startsWith(prefix + "time")) {
 		var date = new Date();
 		var day = date.getDay();
 		var hour = date.getHours();
 		var minute = date.getMinutes();
 		var time = hour.toString() + minute.toString();
-		bot.sendMessage(msg, "The time is " + time + " and the day is " + day);
-        console.log(msg.author.username + ' asked for the time'); // update console
+		message.channel.sendMessage("The time is " + time + " and the day is " + day);
+        console.log(message.author.username + ' asked for the time'); // update console
 	}
 
     // ----- Help
-    if (msg.content.startsWith(prefix + "help")) {
-        var messageSubstring = msg.content.substr(6);
+    if (message.content.startsWith(prefix + "help")) {
+        var messageSubstring = message.content.substr(6);
         var str = messageSubstring.replace('/','');
         if (str.length < 1) {
-            bot.sendMessage(msg, "I am " + bot.user.username + ". I know everything about SWGoH. Try using some of the following commands:" + '\n' + helpLoop());
+            message.channel.sendMessage("I am " + bot.user.username + ". I know everything about SWGoH. Try using some of the following commands:" + '\n' + helpLoop());
         } else if (env.help[str]) {
-            bot.sendMessage(msg, env.help[str]);
+            message.channel.sendMessage(env.help[str]);
         } else {
-            bot.sendMessage(msg, "I don't know that command. Make sure it's spelled correctly and try typing it again. For a list of commands, type \"/help\".");
+            message.channel.sendMessage("I don't know that command. Make sure it's spelled correctly and try typing it again. For a list of commands, type \"/help\".");
         }
-        console.log(msg.author.username + ' asked for help'); // update console
+        console.log(message.author.username + ' asked for help'); // update console
     }
 });
 
 // ----- FUNCTIONS ----- //
 
 // Check day of the week and time against the guild reset time and guild activities
-function dayCheck(msg) {
+function dayCheck(message) {
 	var date = new Date();
 	var day = date.getDay();
 	var hour = date.getHours();
@@ -149,48 +149,48 @@ function dayCheck(msg) {
 
 	if (time > guildReset) {
 		if (day == 0) {
-			bot.sendMessage(msg, rnow + day0 + '\n' + greset + day1);
+			message.channel.sendMessage(rnow + day0 + '\n' + greset + day1);
 		} else if (day == 1) {
-			bot.sendMessage(msg, rnow + day1 + '\n' + greset + day2);
+			message.channel.sendMessage(rnow + day1 + '\n' + greset + day2);
 		} else if (day == 2) {
-			bot.sendMessage(msg, rnow + day2 + '\n' + greset + day3);
+			message.channel.sendMessage(rnow + day2 + '\n' + greset + day3);
 		} else if (day == 3) {
-			bot.sendMessage(msg, rnow + day3 + '\n' + greset + day4);
+			message.channel.sendMessage(rnow + day3 + '\n' + greset + day4);
 		} else if (day == 4) {
-			bot.sendMessage(msg, rnow + day4 + '\n' + greset + day5);
+			message.channel.sendMessage(rnow + day4 + '\n' + greset + day5);
 		} else if (day == 5) {
-			bot.sendMessage(msg, rnow + day5 + '\n' + greset + day6);
+			message.channel.sendMessage(rnow + day5 + '\n' + greset + day6);
 		} else if (day == 6) {
-			bot.sendMessage(msg, rnow + day6 + '\n' + greset + day0);
+			message.channel.sendMessage(rnow + day6 + '\n' + greset + day0);
 		}
 
 	} else {
 		if (day == 1) {
-			bot.sendMessage(msg, rnow + day1 + '\n' + greset + day2);
+			message.channel.sendMessage(rnow + day1 + '\n' + greset + day2);
 		} else if (day == 2) {
-			bot.sendMessage(msg, rnow + day2 + '\n' + greset + day3);
+			message.channel.sendMessage(rnow + day2 + '\n' + greset + day3);
 		} else if (day == 3) {
-			bot.sendMessage(msg, rnow + day3 + '\n' + greset + day4);
+			message.channel.sendMessage(rnow + day3 + '\n' + greset + day4);
 		} else if (day == 4) {
-			bot.sendMessage(msg, rnow + day4 + '\n' + greset + day5);
+			message.channel.sendMessage(rnow + day4 + '\n' + greset + day5);
 		} else if (day == 5) {
-			bot.sendMessage(msg, rnow + day5 + '\n' + greset + day6);
+			message.channel.sendMessage(rnow + day5 + '\n' + greset + day6);
 		} else if (day == 6) {
-			bot.sendMessage(msg, rnow + day6 + '\n' + greset + day0);
+			message.channel.sendMessage(rnow + day6 + '\n' + greset + day0);
 		}  else if (day == 0) {
-			bot.sendMessage(msg, rnow + day0 + '\n' + greset + day1);
+			message.channel.sendMessage(rnow + day0 + '\n' + greset + day1);
 		}
 	}
 }
 
 // Edit the user's message to look up on swgoh.gg
-function characterLookup(msg) {
-	var messageSubstring = msg.content.substr(3);
+function characterLookup(message) {
+	var messageSubstring = message.content.substr(3);
 	var messageTrimmed = messageSubstring.trim();
 	var messageReplaced = messageTrimmed.replace(" ","+");
-	bot.sendMessage(msg, "<http://www.google.com/webhp?#q=" + messageReplaced + "+swgoh.gg+feeling+lucky&btnI>"); // Uses Google's "I'm feeling lucky" to search on swgoh.gg
+	message.channel.sendMessage("<http://www.google.com/webhp?#q=" + messageReplaced + "+swgoh.gg+feeling+lucky&btnI>"); // Uses Google's "I'm feeling lucky" to search on swgoh.gg
 // If the link above does not work, try using the link below
-//        bot.sendMessage(msg, "http://www.google.com/search?ie=UTF-8&oe=UTF-8&sourceid=navclient&gfns=1&q=" + messageReplaced + "+swgoh.gg");
+//        bot.sendMessage("http://www.google.com/search?ie=UTF-8&oe=UTF-8&sourceid=navclient&gfns=1&q=" + messageReplaced + "+swgoh.gg");
 }
 
 // List helpArray and return as string with line breaks
@@ -211,4 +211,4 @@ function output(error) {
                 console.log('%s has successfully logged in.', bot.user.username);
 }
 
-bot.loginWithToken(env.discord.token, output);
+bot.login(env.discord.token);
