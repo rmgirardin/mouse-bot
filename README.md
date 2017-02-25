60a7798ecda5705e226e1199528a2485534d8cb3
Richie <rmgirardin@gmail.com>
2017-02-25 15:37:43 -0500

    1.0.0 update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Mouse Bot
 
-A quite, but helpful Discord bot for Star Wars: Galaxy of Heroes.<br>
+A helpful Discord bot for Star Wars: Galaxy of Heroes.<br>
 This bot uses [discord.js](https://github.com/hydrabolt/discord.js.git) API for node.js.
 
 ## Installation
@@ -8,16 +8,15 @@ This bot uses [discord.js](https://github.com/hydrabolt/discord.js.git) API for 
 To run off of your computer, you'll first need [node 0.12+](https://www.npmjs.com/package/forever/tutorial).
 
 Then install discord.js through the terminal: `npm install discord.js`<br>
-You will also need to install `timer.js` the same way.
 
-Open `config.json.example` and add keys to the blank fields. For `token` add you're bots token from the [Discord App](https://discordapp.com/developers/applications/me) page. Then add the announcements channel ID to `annChannel` and do the same for `officerChannel`. If you do not use an officer channel, you can just put the general channel's ID here. (If you don't know how to find the channel ID, follow [these instructions](https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-server-ID-)) Also add the Twitter keys from the [Twitter Apps](https://apps.twitter.com/). Finally, rename `config.json.example` to `config.json`.
+Open `config.json.example` and add keys to the blank fields. For `token` add you're bots token from the [Discord App](https://discordapp.com/developers/applications/me) page. Then add the announcements channel ID to `generalChannel` and do the same for `annChannel` and `officerChannel`. If you do not use an officer channel, you can just put the general channel's ID here. (If you don't know how to find the channel ID, follow [these instructions](https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-server-ID-)). Finally, rename `config.json.example` to `config.json`.
 
 Now you can use `node app.js` in the terminal in your mouse-bot directory folder. Alternatively you can use `node ` and drag the folder path into the terminal.
 
 ## Functionality
 
 #### New Members Added to Server
-The bot recognizes when new users are added to the server and greets then with a custom greeting. In this case, it greets the user and tells them to review the `#rules` channel.
+The bot recognizes when new users are added to the server and greets then with a custom greeting. By default, Mouse Bot greets the user and tells them to review the `#rules` channel.
 
 #### Commands
 
@@ -29,22 +28,16 @@ The bot recognizes when new users are added to the server and greets then with a
 - /ch *(character name)*
     - this will look up the character information on swgoh.gg
     - Mouse bot returns a link using Google's "I'm Feeling Lucky" feature
-- /rancor
-    - we have a 0 damage policy for the first 24 hours. I implemented this to remind everyone about this rule when the command is first invoked and then announce when 24 hours is over so that everyone can begin attacking. Mouse bot reminds users in the `#announcements` channel
-- /tstart *(#)*
-    - begins a timer for that # of minutes
 - /rules
     - tells you to visit the `#rules` channel
-
-#### Forum Feed Updates
-~~For this I use [IFTTT](http://ifttt.com) to tweet the post titles and links of [game](https://forums.galaxy-of-heroes.starwars.ea.com/categories/game-updates)/[developer](https://forums.galaxy-of-heroes.starwars.ea.com/categories/developer-updates) updates from the SWGoH forum to a twitter account. Using a twitter application, the bot streams the twitter feed with the help of `user-stream`.~~
-
-This is now on the to-do and hopeful will be updated and working soon.
+- /convert
+    - Converts the percent amount to damage in raids
+    - Only works for all of the phases of Rancor Tier 7 right now.
 
 ## To-Do
 
 - Add forum feed updater
-- Damage amount randomizer in raid message
+- Clean and consolidate code
 
 ## Help
 If you need help, you can always ask me.
--- index.js
+++ index.js
@@ -1,23 +1,8 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 const env = require('./config.json');
 
-// Initialize timer
-const Timer = require('timer.js');
-var timer = new Timer({
-    tick: 30,
-    onstart : function() {
-    },
-    onstop : function() {
-        console.log('The timer has been stopped'); // update console
-        bot.sendMessage(env.discord.officerChannel, "For some reason, your timer has been stopped.")
-    },
-    onend : function() {
-        console.log('The timer has ended'); // update console
-    }
-});
-
-// Defining the variables
+// Defining the variables for the dailies
 const guildReset = env.guildReset;
 const rnow = "Right now it's ";
 const greset = "Later will be ";
@@ -28,44 +13,85 @@ const day3 = "**Galactic War Battles**";
 const day4 = "**Hard Mode Battles**";
 const day5 = "**Challenges**";
 const day6 = "**Dark Side Battles**";
+const p1PitDamage = 18725;
+const p2PitDamage = 30063;
+const p3PitDamage = 32889;
+const p4PitDamage = 21230;
 
-const helpArray = [
+// Help Array for /help command
+var helpArray = [
     "/dailies",
-    "/ch *(character name)*",
-    "/rancor",
-    "/stimer *(#)*",
-    "/help *(command)*"]
+    "/ch (character name)",
+    "/convert __% (raid and phase)",
+    "/help (command)"];
 
-// Connecting Mouse Bot
-bot.on('ready', () => {
-	console.log("%s is servering %s channel(s) over %s server(s)", bot.user.username, bot.channels.size, bot.guilds.size);
+// ----- Connecting Mouse Bot ----- //
+client.on('ready', () => {
+    client.channels.get(env.discord.generalChannel).sendMessage("<Vrrrrrvd tkk tkkdtk>");
+	console.log("%s is servering %s channel(s) over %s server(s)", client.user.username, client.channels.size, client.guilds.size);
 });
 
 // When the Mouse Bot disconnects
-bot.on('disconnected', () => {
-	console.log("NOOOOOOOO!! I've been disconnected!");
+client.on('disconnected', () => {
+    client.channels.get(env.discord.generalChannel).sendMessage("<RRRRDDTT!!!! Wewewedt! Veeeeedt!>");
+	console.log("%s has powered down", client.user.username);
 	process.exit(1); //exit node.js with an error
 });
 
-// Welcomes new members to the server
-bot.on('guildMemberAdd', (guild, member) => {
-  guild.defaultChannel.sendMessage("Welcome, " + member.nickname + ". Please make sure to read the <#" + env.discord.rulesChannel + ">. If you need help, use \"/help\" and I'll assist.");
-  console.log(member.nickname + ' joined the server: ' + server); // update console
+// Welcome new members to the server
+client.on('guildMemberAdd', (member) => {
+    const guild = member.guild;
+    guild.channels.get(guild.id).sendMessage("Welcome, " + member.nickname + ". Please make sure to read the <#" + env.discord.rulesChannel + ">. If you need help, use \"/help\" and I'll assist.");
+    console.log(member.nickname + ' joined the server: ' + server); // update console when member joins
 });
 
 // ----- /COMMANDS ----- //
 
-bot.on('message', message => {
+client.on('message', message => {
 
     // Setting and checking for prefix
     const prefix = "/";
     if(!message.content.startsWith(prefix)) return;
-    if(message.author.bot) return;
+
+    if(message.author.bot) return; //Prevent botception
+
+    // ----- Raid Percent Converter
+    breakme: if (message.content.startsWith(prefix + 'convert')) {
+        extractNumber(message);
+            if (message.content.includes('pit') || message.content.includes('rancor')) {
+            var raid = "pit";
+            if (message.content.includes('p1') || message.content.includes('phase1')) {
+                var phase = "1";
+                var damageInt = p1PitDamage*eNum;
+                convertionMessage(message, eNum, damageInt, phase, raid);
+            } if (message.content.includes('p2') || message.content.includes('phase2')) {
+                var phase = "2";
+                var damageInt = p2PitDamage*eNum;
+                convertionMessage(message, eNum, damageInt, phase, raid);
+            } if (message.content.includes('p3') || message.content.includes('phase3')) {
+                var phase = "3";
+                var damageInt = p3PitDamage*eNum;
+                convertionMessage(message, eNum, damageInt, phase, raid);
+            } if (message.content.includes('p4') || message.content.includes('phase4')) {
+                var phase = "4";
+                var damageInt = p4PitDamage*eNum;
+                convertionMessage(message, eNum, damageInt, phase, raid);
+            }
+            break breakme;
+        } if (message.content.includes('tank') || message.content.includes('AAT') || message.content.includes('HAAT')) {
+            message.channel.sendMessage("I don't have any information on the damage of the Tank Takedown Heroic raid yet.");
+            console.log(message.author.username + ' asked me to convert percent to damage for the Tank Takedown raid'); // update console
+            break breakme;
+        } else {
+            message.channel.sendMessage("I can help you convert raid percentage to damage. Use the following format: `/convert __%  pit  p1`");
+            console.log('I told ' + message.author.username + ' how to convert raid percentage to damage.') // update console
+        }
+    }
 
 	// ----- Dailies
 	if (message.content.startsWith(prefix + 'dailies')) {
 		dayCheck(message);
-		console.log('Gave ' + message.author.username + ' the dailies update'); // update console
+		console.log("Gave " + message.author.username + " the dailies update"); //update console
 	}
 
 	// ----- Character Seach
@@ -74,72 +100,63 @@ bot.on('message', message => {
 		console.log(message.author.username + ' asked me to search for character information'); // update console
 	}
 
-    // ----- Rancor Raid Timer and Notifier
-    if (message.content.startsWith(prefix + 'rancor')) {
-        timer.start(env.rancorTimer*3600); // convert hours to seconds
-        timer.on('start', function() {
-            message.guild.channels.find('name', 'announcements').sendMessage("The Rancor Raid has been started. Only do 0 damage for 24 hours. See the <#" + env.discord.rulesChannel + ">");
-        })
-        timer.on('end', function() {
-            message.guild.channels.find('name', 'announcements').sendMessage("It's been 24 hours since the raid has started. Everyone can attack like normal!");
-        })
-        console.log(message.author.username + ' has started the Rancor Raid timer') // update console
-    }
-
-    // ----- Timer
-    if (message.content.startsWith(prefix + 'tstart')) {
-        var messageSubstring = message.content.substr(7);
-        var messageTrimmed = messageSubstring.trim();
-        var minutesToSeconds = 60*parseInt(messageTrimmed);
-        timer.start(parseInt(minutesToSeconds));
-        message.channel.sendMessage("A timer has been started for " + messageTrimmed + " minute(s).");
-        timer.on('end', function() {
-            message.channel.sendMessage("*DING! DING! DING!*" + '\n' + "Your timer has ended.");
-        })
-        console.log(message.author.username + ' started a timer'); // update console
-    }
-
 	// ----- Rules
-	if (message.content.startsWith(prefix + "rules")) {
-		message.channel.sendMessage("The rules are updated in the <#" + env.discord.rulesChannel + "> channel.");
+	if (message.content.startsWith(prefix + 'rules')) {
+		message.channel.sendMessage("The rules are located in the <#" + env.discord.rulesChannel + "> channel.");
 		console.log(message.author.username + " asked me about the rules"); // update console
 	}
 
 	// ----- What's my name?
-	if (message.content.startsWith(prefix + "name")) {
+	if (message.content.startsWith(prefix + 'name')) {
 		message.channel.sendTTSMessage("I love it when you call me big pop-pa!");
-		console.log("I told " + message.author.username + " what I like to be called"); // update console
+		console.log("I told " + message.author.username + " what I like to be called"); //update console
 	}
 
 	// ----- Time
-	if (message.content.startsWith(prefix + "time")) {
+	if (message.content.startsWith(prefix + 'time')) {
 		var date = new Date();
 		var day = date.getDay();
 		var hour = date.getHours();
 		var minute = date.getMinutes();
 		var time = hour.toString() + minute.toString();
 		message.channel.sendMessage("The time is " + time + " and the day is " + day);
-        console.log(message.author.username + ' asked for the time'); // update console
+        console.log(message.author.username + ' asked for the time'); //update console
 	}
 
     // ----- Help
-    if (message.content.startsWith(prefix + "help")) {
+    if (message.content.startsWith(prefix + 'help')) {
         var messageSubstring = message.content.substr(6);
         var str = messageSubstring.replace('/','');
         if (str.length < 1) {
-            message.channel.sendMessage("I am " + bot.user.username + ". I know everything about SWGoH. Try using some of the following commands:" + '\n' + helpLoop());
+            message.channel.sendMessage("Try using some of the following commands:```" + '\n' + helpLoop() + "```");
         } else if (env.help[str]) {
             message.channel.sendMessage(env.help[str]);
         } else {
             message.channel.sendMessage("I don't know that command. Make sure it's spelled correctly and try typing it again. For a list of commands, type \"/help\".");
         }
-        console.log(message.author.username + ' asked for help'); // update console
+        console.log(message.author.username + ' asked for help on ' + str); //update console
     }
 });
 
 // ----- FUNCTIONS ----- //
 
-// Check day of the week and time against the guild reset time and guild activities
+// ----- Edit the user's message to look up on swgoh.gg
+function characterLookup(message) {
+	var messageSubstring = message.content.substr(3);
+	var messageTrimmed = messageSubstring.trim();
+	var messageReplaced = messageTrimmed.replace(" ","+");
+	message.channel.sendMessage("<http://www.google.com/webhp?#q=" + messageReplaced + "+swgoh.gg+feeling+lucky&btnI>"); // Uses Google's "I'm feeling lucky" to search on swgoh.gg
+// If the link above does not work, try using the link below
+//        message.channel.sendMessage("http://www.google.com/search?ie=UTF-8&oe=UTF-8&sourceid=navclient&gfns=1&q=" + messageReplaced + "+swgoh.gg");
+}
+
+// ----- Sends the convertion command message
+function convertionMessage(message, eNum, damageInt, phase, raid) {
+    message.channel.sendMessage(eNum + "% is about " + damageInt.toLocaleString() + " damage in Phase " + phase + " of the " + raid + " raid.");
+    console.log(message.author.username + ' asked me to convert percent to damage for Phase ' + phase + ' of the ' + raid + ' raid.'); // update console
+}
+
+// ----- Check day of the week and time against the guild reset time and guild activities
 function dayCheck(message) {
 	var date = new Date();
 	var day = date.getDay();
@@ -183,17 +200,19 @@ function dayCheck(message) {
 	}
 }
 
-// Edit the user's message to look up on swgoh.gg
-function characterLookup(message) {
-	var messageSubstring = message.content.substr(3);
-	var messageTrimmed = messageSubstring.trim();
-	var messageReplaced = messageTrimmed.replace(" ","+");
-	message.channel.sendMessage("<http://www.google.com/webhp?#q=" + messageReplaced + "+swgoh.gg+feeling+lucky&btnI>"); // Uses Google's "I'm feeling lucky" to search on swgoh.gg
-// If the link above does not work, try using the link below
-//        bot.sendMessage("http://www.google.com/search?ie=UTF-8&oe=UTF-8&sourceid=navclient&gfns=1&q=" + messageReplaced + "+swgoh.gg");
+// ----- Extracts the number from a string and assigns it to the variable "eNum"
+function extractNumber(message) {
+    var messageArray = message.toString().split(' ');
+    for (i = 0; i < messageArray.length; i++) {
+        j = parseInt(messageArray[i]);
+        if (!isNaN(j)) {
+            eNum = j;
+            return eNum;
+        }
+    }
 }
 
-// List helpArray and return as string with line breaks
+// ----- List helpArray and return as string with line breaks
 function helpLoop() {
 	var text = "";
 	for (i = 0; i < helpArray.length; i++) {
@@ -202,13 +221,4 @@ function helpLoop() {
 	return text;
 }
 
-// Login output
-function output(error) {
-        if (error) {
-                console.log('There was an error logging in: %s', error);
-                return;
-        } else
-                console.log('%s has successfully logged in.', bot.user.username);
-}
-
-bot.login(env.discord.token);
+client.login(env.discord.token);
