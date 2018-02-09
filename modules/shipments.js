60aec3c8e85e4af0321eb6a0bd3c4b5d528db1ae
Richie <rmgirardin@gmail.com>
2018-02-09 12:50:14 -0500

    Updated shipment images

--- modules/shipments.js
+++ modules/shipments.js
@@ -1,201 +1,30 @@
 const shipments = {
 
     "arena": {
-        "characters": [
-            "admiral-ackbar",
-            "asajj-ventress",
-            "cassian-andor",
-            "chief-nebit",
-            "coruscant-underworld-police",
-            "darth-sidious",
-            "eeth-koth",
-            "grand-moff-tarkin",
-            "greedo",
-            "hk-47",
-            "ig-88",
-            "kanan-jarrus",
-            "mace-windu",
-            "nute-gunray",
-            "princess-leia",
-            "savage-opress",
-            "stormtrooper-han",
-            "ugnaught"
-        ],
-        "ships": [
-
-        ],
         "image": "https://swgoh.gg/static/img/assets/tex.icon_pvp_credit.png"
     },
 
     "cantina": {
-        "characters": [
-            "ahsoka-tano",
-            "bistan",
-            "boba-fett",
-            "chopper",
-            "ct-5555-fives",
-            "first-order-officer",
-            "hoth-rebel-scout",
-            "jawa-scavenger",
-            "jedi-knight-guardian",
-            "mob-enforcer",
-            "old-daka",
-            "pao",
-            "poe-dameron",
-            "qui-gon-jinn",
-            "stormtrooper"
-        ],
-        "ships": [
-            "cassians-u-wing"
-        ],
         "image": "https://swgoh.gg/static/img/assets/tex.icon_force_pts.png"
     },
 
     "fleet": {
-        "characters": [
-            "admiral-ackbar",
-            "ahsoka-tano",
-            "ahsoka-tano-fulcrum",
-            "biggs-darklighter",
-            "bistan",
-            "boba-fett",
-            "cc-2224-cody",
-            "chirrut-imwe",
-            "clone-sergeant-phase-i",
-            "ct-21-0408-echo",
-            "ct-5555-fives",
-            "ct-7567-rex",
-            "darth-maul",
-            "darth-vader",
-            "ezra-bridger",
-            "first-order-tie-pilot",
-            "general-grievous",
-            "geonosian-soldier",
-            "geonosian-spy",
-            "grand-moff-tarkin",
-            "jedi-consular",
-            "mace-windu",
-            "plo-koon",
-            "poe-dameron",
-            "resistance-pilot",
-            "scarif-rebel-pathfinder",
-            "sun-fac",
-            "tie-fighter-pilot",
-            "tusken-shaman",
-            "wedge-antilles",
-            "zam-wesell"
-        ],
-        "ships": [
-            "ahsoka-tanos-jedi-starfighter",
-            "biggs-darklighters-x-wing",
-            "bistans-u-wing",
-            "clone-sergeants-arc-170",
-            "first-order-tie-fighter",
-            "gauntlet-starfighter",
-            "geonosian-soldiers-starfighter",
-            "geonosian-spys-starfighter",
-            "ghost",
-            "imperial-tie-fighter",
-            "jedi-consulars-starfighter",
-            "phantom-ii",
-            "plo-koons-jedi-starfighter",
-            "poe-damerons-x-wing",
-            "resistance-x-wing",
-            "rexs-arc-170",
-            "scimitar",
-            "slave-i",
-            "sun-facs-geonosian-starfighter",
-            "tie-advanced-x1",
-            "umbaran-starfighter",
-            "wedge-antilless-x-wing"
-        ],
         "image": "https://swgoh.gg/static/img/assets/tex.icon_pvp_credit_ship.png"
     },
 
     "galactic war": {
-        "characters": [
-            "biggs-darklighter",
-            "bodhi-rook",
-            "cad-bane",
-            "captain-phasma",
-            "dathcha",
-            "garazeb-zeb-orrelios",
-            "ig-86-sentinel-droid",
-            "k-2so",
-            "luminara-unduli",
-            "magmatrooper",
-            "nightsister-initiate",
-            "poggle-the-lesser",
-            "resistance-pilot",
-            "teebo",
-            "tusken-raider"
-        ],
-        "ships": [
-            "ahsoka-tanos-jedi-starfighter",
-            "biggs-darklighters-x-wing",
-            "clone-sergeants-arc-170",
-            "first-order-tie-fighter",
-            "geonosian-soldiers-starfighter",
-            "geonosian-spys-starfighter",
-            "imperial-tie-fighter",
-            "jedi-consulars-starfighter",
-            "plo-koons-jedi-starfighter",
-            "resistance-x-wing",
-            "sun-facs-geonosian-starfighter",
-            "umbaran-starfighter",
-            "wedge-antilless-x-wing"
-        ],
         "image": "https://swgoh.gg/static/img/assets/tex.icon_galactic_currency.png"
     },
 
     "guild": {
-        "characters": [
-            "aayla-secura",
-            "b2-super-battle-droid",
-            "barriss-offee",
-            "cc-2224-cody",
-            "clone-sergeant-phase-i",
-            "colonel-starck",
-            "ct-21-0408-echo",
-            "ct-7567-rex",
-            "darth-maul",
-            "dengar",
-            "ewok-elder",
-            "finn",
-            "gamorrean-guard",
-            "ig-100-magnaguard",
-            "ima-gun-di",
-            "jawa-engineer",
-            "jyn-erso",
-            "kit-fisto",
-            "kylo-ren",
-            "logray",
-            "luke-skywalker-farmboy",
-            "nightsister-acolyte",
-            "obi-wan-kenobi-old-ben",
-            "paploo",
-            "resistance-trooper",
-            "rey-scavenger",
-            "sith-assassin",
-            "snowtrooper",
-            "sun-fac",
-            "talia",
-            "tusken-shaman",
-            "zam-wesell"
-        ],
-        "ships": [
-
-        ],
         "image": "https://swgoh.gg/static/img/assets/tex.guild_currency.png"
     },
 
-    "shard": {
-        "characters": [
-            "general-grievous"
-        ],
-        "ships": [
+    "guild events": {
+        "image": "https://swgoh.gg/static/img/assets/tex.icon_territory_credit.png"
+    },
 
-        ],
+    "shard": {
         "image": "https://swgoh.gg/static/img/assets/tex.shard_currency.png"
     },
 
