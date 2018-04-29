5fc3e11fb3ec33ee758b0103d1ca4f5b752e0366
Richie <rmgirardin@gmail.com>
2018-04-29 11:03:54 -0400

    Added buglog to sql db

--- commands/bug.js
+++ commands/bug.js
@@ -6,12 +6,11 @@ exports.run = async (client, message, cmd, args, level) => { // eslint-disable-l
 
         if (!args[0]) return client.cmdError(message, cmd);
 
-        const settings = client.settings.get(message.guild.id);
         const report = args.join(" ");
 
         // Instead of using the cmd.help.name, we'll use the action that was
         // actually called
-        const command = message.content.slice(settings.prefix.length).trim().split(/ +/g).shift().toLowerCase();
+        const command = message.content.slice(message.settings.prefix.length).trim().split(/ +/g).shift().toLowerCase();
 
         const embed = new RichEmbed()
             .setAuthor(message.author.tag, message.author.avatarURL)
@@ -24,10 +23,17 @@ exports.run = async (client, message, cmd, args, level) => { // eslint-disable-l
         if (command === "bug" || command === "bugs") embed.setColor(0xE68D60);
         if (command === "suggestion" || command === "suggestions") embed.setColor(0x009800);
 
+        // Here we'll send the report to the Bot Owner and also to the botlog database
         try {
+            const permLevel = client.config.permLevels.find(l => l.level === level).name;
             await client.users.get(client.config.ownerID).send({embed});
+            await client.doSQL(
+                "INSERT INTO buglog (timestamp, type, userId, permLevel, report) VALUES (?, ?, ?, ?, ?)",
+                [new Date(), command, message.author.id.toString(), permLevel, report]
+            );
         } catch (error) {
-            client.logger.error(client, `Couldn't send bug to Necavit:\n${error.stack}`);
+            client.errlog(cmd, message, level, error);
+            client.logger.error(client, `Couldn't send bug to Necavit\n${error.stack}`);
         }
         await message.reply(`I just sent **Necavit#0540** the **${command}**! Thank you for your feedback!`);
 
@@ -40,6 +46,7 @@ exports.run = async (client, message, cmd, args, level) => { // eslint-disable-l
         client.addPoints(message, client.config.messagePoints*2);
 
     } catch (error) {
+        client.errlog(cmd, message, level, error);
         client.logger.error(client, `bug command failure:\n${error.stack}`);
         client.codeError(message);
     }
