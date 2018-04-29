f126f2dd89ff7fad453a67751837a6640216ac7d
Richie <rmgirardin@gmail.com>
2018-04-29 15:03:51 -0400

    Added registered users statistic

--- commands/stats.js
+++ commands/stats.js
@@ -10,13 +10,18 @@ exports.run = async (client, message, cmd, args, level) => { // eslint-disable-l
 
         const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
 
+        const profiles = await client.doSQL("SELECT COUNT(*) FROM profiles");
+
         const embed = new Discord.RichEmbed()
             .setTitle(`${client.user.username.toProperCase()} Statistics`)
             .setColor(0x268BD2)
             .setDescription(`**${pjson.name.replace("-", " ").toProperCase()}** v${pjson.version}  |  **Node** ${process.version}  |  **Discord.js** v${Discord.version}`)
             .addField("Servers:", client.guilds.size.toLocaleString(), true)
             .addField("Channels:", client.channels.size.toLocaleString(), true)
+            .addBlankField(true)
             .addField("Users:", client.users.size.toLocaleString(), true)
+            .addField("Registered:", profiles[0]["COUNT(*)"], true)
+            .addBlankField(true)
             .addField("Mem Usage:", `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`, true)
             .addField("CPU Load:", `${Math.round(os.loadavg()[0]*10000)/100}%`, true)
             .addField("Uptime:", duration, true);
