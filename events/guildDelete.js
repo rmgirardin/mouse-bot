94f7f7a8bc1083c52da2139ec42559785859242c
Richie <rmgirardin@gmail.com>
2018-05-17 19:49:02 -0400

    Removed the server reset configs when a guild is removed

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -4,6 +4,5 @@ module.exports = (client, guild) => {
 
     client.logger.log(client, `${guild.name} (${guild.id}) server was removed`);
     client.botLogEmbed(client, `I was removed from the server ${guild.name}`, "Guild Delete", 0xFF1900); // Bot-Server Log
-    client.settings.delete(guild.id);
 
 };
